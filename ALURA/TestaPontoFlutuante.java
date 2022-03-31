d0e42331a2d8fd773f38f97869ad74f6fbaa2ee1
Nahuan de Abreu Silva <80486629+Nahuan-Abreu@users.noreply.github.com>
2022-03-31 14:51:12 -0300

    Rename TestaPontoFlutuante.java to ALURA/TestaPontoFlutuante.java

--- ALURA/TestaPontoFlutuante.java
+++ ALURA/TestaPontoFlutuante.java
@@ -3,7 +3,7 @@ public class TestaPontoFlutuante {
 	public static void main(String[] args) {
 		double salario;
 		salario = 1250.70;
-		System.out.println("meu sal·rio È " + salario);
+		System.out.println("meu sal√°rio √© " + salario);
 		
 		double divisao = 3.14/2;
 		System.out.println(divisao);
